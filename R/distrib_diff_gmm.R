0667e8eb06a4349c7844ccc93da537d7a1cdb017
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2024-04-17 13:04:22 +0100

    distrib_diff_gmm: decided on checkpoints etc

--- R/distrib_diff_gmm.R
+++ R/distrib_diff_gmm.R
@@ -63,15 +63,13 @@ distrib_diff_gmm_g <- function(x, z_g, mu_g, Sigma_g) {
 
   mahala_ewcdf_g_func <- spatstat.geom::ewcdf(scaled_mahalas_g, w_g)
 
-  # checkpoints <- sort(scaled_mahalas_g)
-  checkpoints <- stats::qbeta(seq(0.001, 0.999, 0.001),
-                              var_num / 2, (n_g - var_num - 1) / 2)
+  check_seq <- seq(0.001, 0.999, 0.001)
+  checkpoints <- stats::qbeta(check_seq, var_num / 2, (n_g - var_num - 1) / 2)
 
   mahala_ewcdf_g <- mahala_ewcdf_g_func(checkpoints)
   beta_cdf_g <- stats::pbeta(checkpoints, var_num / 2, (n_g - var_num - 1) / 2)
 
-  distrib_diff_g <- sum(abs(mahala_ewcdf_g - beta_cdf_g)) / obs_num
-  # distrib_diff_g <- max(abs(mahala_ewcdf_g - beta_cdf_g))
+  distrib_diff_g <- mean(abs(mahala_ewcdf_g - beta_cdf_g))
 
   dens_g_factor <- (2 * pi)^(- 0.5 * var_num) * det(Sigma_g)^(-0.5)
   dens_g <- dens_g_factor * exp(- 0.5 * mahalas_g)
