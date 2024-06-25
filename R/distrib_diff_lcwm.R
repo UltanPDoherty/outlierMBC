9b3e4fa9a161c8e2b0d3713d6d46699351e8c33f
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2024-06-25 09:20:01 +0100

    Created ombc_lcwm

---
+++ R/distrib_diff_lcwm.R
@@ -0,0 +1,117 @@
+#' distrib_diff_lcwm
+#'
+#' @param x Data.
+#' @param z Component assignment probability matrix.
+#' @param prop Vector of component proportions.
+#' @param mu List of component mean vectors.
+#' @param sigma List of component covariance matrices.
+#'
+#' @return List of
+#' * distrib_diff
+#' * distrib_diff_vec
+#' * choice_id
+distrib_diff_lcwm <- function(x, z, prop, mu, sigma, mod_list, y_sigma, alpha = 0.5) {
+  obs_num <- nrow(x)
+  comp_num <- ncol(z)
+
+  distrib_diff_vec <- c()
+  scaled_mahalas <- matrix(nrow = obs_num, ncol = comp_num)
+  dens_mat <- matrix(nrow = obs_num, ncol = comp_num)
+  for (g in seq_len(comp_num)) {
+    out_g <- distrib_diff_lcwm_g(
+      x, z[, g],
+      mu[, g, drop = FALSE],
+      as.matrix(sigma[, , g]),
+      mod_list[[g]],
+      y_sigma[g],
+      alpha
+    )
+    distrib_diff_vec[g] <- out_g$distrib_diff_g
+    scaled_mahalas[, g] <- out_g$scaled_mahalas_g
+    dens_mat[, g] <- out_g$dens_g
+  }
+
+  mix_dens <- dens_mat %*% prop
+
+  choice_id <- which.min(mix_dens)
+
+  distrib_diff <- sum(prop * distrib_diff_vec)
+
+  return(list(
+    distrib_diff = distrib_diff,
+    distrib_diff_vec = distrib_diff_vec,
+    choice_id = choice_id
+  ))
+}
+
+#' distrib_diff_lcwm_g
+#'
+#' @param x Data.
+#' @param z_g Component assignment probability vector.
+#' @param mu_g Component mean vector.
+#' @param sigma_g Component covariance matrix.
+#'
+#' @return List of
+#' * distrib_diff_g
+#' * scaled_mahalas_g
+#' * dens_x_g
+distrib_diff_lcwm_g <- function(x, z_g, mu_g, sigma_g, mod_g, y_sigma_g, alpha = 0.5) {
+  var_num <- ncol(x)
+  n_g <- sum(z_g)
+
+  eps <- 0.001
+  check_seq <- seq(eps, 1 - eps, eps)
+
+  # --------
+
+  # df_g <- mod_g$df.residual
+  df_g <- round(n_g) - 2
+
+  hat_g <- stats::hatvalues(mod_g)
+
+  # student_resids_g2 <- stats::rstandard(mod_g)
+  student_resids_g <- mod_g$residuals / (y_sigma_g * sqrt(1 - hat_g))
+# browser()
+  scsqst_res_g <- student_resids_g^2 / df_g
+
+  checkpoints_y <- stats::qbeta(check_seq, 1 / 2, (df_g - 1) / 2)
+
+  scsqst_res_ecdf_g_func <- spatstat.geom::ewcdf(scsqst_res_g, z_g / n_g)
+
+  scsqst_res_ecdf_g <- scsqst_res_ecdf_g_func(checkpoints_y)
+  beta_cdf_y_g <- stats::pbeta(checkpoints_y, 1 / 2, (df_g - 1) / 2)
+  distrib_diff_y_g <- mean(abs(scsqst_res_ecdf_g - beta_cdf_y_g))
+
+  dens_y_g <- stats::dnorm(mod_g$residuals, mean = 0, sd = y_sigma_g)
+
+  # ----------
+
+  checkpoints_x <- stats::qbeta(check_seq, var_num / 2, (n_g - var_num - 1) / 2)
+
+  mahalas_g <- stats::mahalanobis(x, mu_g, (n_g / (n_g - 1)) * sigma_g)
+  scaled_mahalas_g <- ((n_g) / (n_g - 1)^2) * mahalas_g
+  mahala_ewcdf_g_func <- spatstat.geom::ewcdf(scaled_mahalas_g, z_g / n_g)
+
+  mahala_ewcdf_g <- mahala_ewcdf_g_func(checkpoints_x)
+  beta_cdf_x_g <- stats::pbeta(checkpoints_x, var_num / 2, (n_g - var_num - 1) / 2)
+  distrib_diff_x_g <- mean(abs(mahala_ewcdf_g - beta_cdf_x_g))
+
+  dens_x_g <- (2 * pi)^(-var_num / 2) * det(sigma_g)^(-0.5) * exp(-mahalas_g / 2)
+
+  # ------------
+
+  distrib_diff_g <- sqrt(alpha * distrib_diff_x_g^2 + (1 - alpha) * distrib_diff_y_g^2)
+  # distrib_diff_g <- alpha * distrib_diff_x_g + (1 - alpha) * distrib_diff_y_g
+
+  dens_g <- dens_x_g * dens_y_g
+  # dens_g <- dens_x_g
+  # dens_g <- dens_y_g
+
+  # cat(paste0("x: ", distrib_diff_x_g, ", y:", distrib_diff_y_g, "\n"))
+
+  return(list(
+    distrib_diff_g = distrib_diff_g,
+    scaled_mahalas_g = scaled_mahalas_g,
+    dens_g = dens_g
+  ))
+}
---
+++ R/ombc_lcwm.R
@@ -0,0 +1,141 @@
+#' ombc_lcwm
+#'
+#' @description
+#' Iterative Detection & Identification of Outliers for a Gaussian lcwmture Model
+#'
+#'
+#' @param x Data.
+#' @param comp_num Number of components.
+#' @param max_out Maximum number of outliers.
+#' @param mnames Model names for flexCWM::cwm.
+#' @param seed Seed.
+#' @param print_interval How frequently the iteration count is printed.
+#' @param alpha Factor to control relative importance of covariate and response.
+#'
+#' @return List of
+#' * distrib_diffs
+#' * outlier_bool
+#' * outlier_num
+#' * outlier_rank
+#' * gmm_labels
+#' @export
+#'
+#' @examples
+#' ombc_p1 <- simulate_ombc(
+#' n = c(1000, 1000),
+#' mu = list(c(-1), c(+1)),
+#' sigma = list(as.matrix(0.2), as.matrix(0.2)),
+#' beta = list(c(1, 0), c(1, 3)),
+#' error_sd = c(1, 1),
+#' outlier_num = c(25, 25),
+#' outlier_type = "x_and_y",
+#' seed = 123,
+#' crit_val = 0.9999,
+#' range_multipliers = c(1.5, 2)
+#' )
+#'
+#' ombc_p1_lcwm <- ombc_lcwm(
+#' xy = ombc_p1[, -3],
+#' x = ombc_p1$X1,
+#' formulaY = Y ~ X1,
+#' comp_num = 2,
+#' max_out = 200,
+#' mnames = "V",
+#' seed = 123,
+#' print_interval = 100,
+#' alpha = 0.5
+#' )
+#'
+#' plot(
+#' ombc_p1[, c("X1", "Y")], pch = ombc_p1$G + 1, col = ombc_p1_lcwm$gmm_labels
+#' )
+ombc_lcwm <- function(xy, x, formulaY, comp_num, max_out, mnames = "VVV", seed = 123,
+                     print_interval = Inf, alpha = 0.5) {
+  xy0 <- xy
+  x <- as.matrix(x)
+  x0 <- x
+
+  var_num <- ncol(x)
+
+  z0 <- NULL
+  cwm_init <- "kmeans"
+
+  distrib_diffs <- c()
+  outlier_rank <- rep(0, nrow(x))
+  for (i in seq_len(max_out + 1)) {
+    if (i %% print_interval == 0) cat("i = ", i, "\n")
+
+    set.seed(seed)
+    invisible(capture.output(lcwm <- flexCWM::cwm(
+      formulaY = formulaY,
+      familyY = gaussian(link = "identity"),
+      data = xy,
+      Xnorm = x,
+      modelXnorm = mnames,
+      k = comp_num,
+      initialization = cwm_init,
+      start.z = z0,
+      seed = seed
+    )))
+
+    prop <- colSums(lcwm$models[[1]]$posterior)
+
+    if (any(prop < (var_num + 1))) {
+      warning(paste0(
+        "One of the components became too small after removing ",
+        i - 1, " outliers."
+      ))
+      break()
+    }
+
+    mod_list <- lapply(lcwm$models[[1]]$GLModel, function(x) x$model)
+    names(mod_list) <- paste0("comp.", seq_along(mod_list))
+    y_sigma <- vapply(lcwm$models[[1]]$GLModel, function(x) x$sigma, double(1L))
+
+    out <- distrib_diff_lcwm(
+      x,
+      lcwm$models[[1]]$posterior,
+      prop,
+      lcwm$models[[1]]$concomitant$normal.mu,
+      lcwm$models[[1]]$concomitant$normal.Sigma,
+      mod_list,
+      y_sigma,
+      alpha
+    )
+
+    distrib_diffs[i] <- out$distrib_diff
+
+    outlier_rank[!outlier_rank][out$choice_id] <- i
+    x <- x[-out$choice_id, , drop = FALSE]
+    xy <- xy[-out$choice_id, , drop = FALSE]
+    z0 <- lcwm$models[[1]]$posterior[-out$choice_id, , drop = FALSE]
+    cwm_init <- "manual"
+  }
+
+  outlier_num <- which.min(distrib_diffs) - 1
+
+  outlier_bool <- outlier_rank <= outlier_num & outlier_rank != 0
+
+  set.seed(seed)
+  lcwm <- flexCWM::cwm(
+    formulaY = formulaY,
+    familyY = gaussian(link = "identity"),
+    data = xy0[!outlier_bool, ],
+    Xnorm = x0[!outlier_bool, ],
+    modelXnorm = mnames,
+    k = comp_num,
+    initialization = "kmeans",
+    seed = seed
+  )
+
+  gmm_labels <- rep(1, nrow(x0))
+  gmm_labels[!outlier_bool] <- 1 + lcwm$models[[1]]$cluster
+
+  return(list(
+    distrib_diffs = distrib_diffs,
+    outlier_bool = outlier_bool,
+    outlier_num = outlier_num,
+    outlier_rank = outlier_rank,
+    gmm_labels = gmm_labels
+  ))
+}
