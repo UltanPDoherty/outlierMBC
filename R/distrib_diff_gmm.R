ac73b558a01e9aec8f32387c6fad782a9ad08f3d
UltanPDoherty <72346121+UltanPDoherty@users.noreply.github.com>
2024-06-26 15:45:36 +0100

    Deleting distrib_diff_gmm_g

--- R/distrib_diff_gmm.R
+++ R/distrib_diff_gmm.R
@@ -15,13 +15,11 @@ distrib_diff_gmm <- function(x, z, prop, mu, sigma) {
   comp_num <- ncol(z)
 
   distrib_diff_vec <- c()
-  scaled_mahalas <- matrix(nrow = obs_num, ncol = comp_num)
   dens_mat <- matrix(nrow = obs_num, ncol = comp_num)
   for (g in seq_len(comp_num)) {
-    out_g <- distrib_diff_gmm_g(x, z[, g], mu[[g]], sigma[[g]])
-    distrib_diff_vec[g] <- out_g$distrib_diff_g
-    scaled_mahalas[, g] <- out_g$scaled_mahalas_g
-    dens_mat[, g] <- out_g$dens_g
+    out_g <- distrib_diff_mahalanobis(x, z[, g], mu[[g]], sigma[[g]])
+    distrib_diff_vec[g] <- out_g$diff
+    dens_mat[, g] <- out_g$dens
   }
 
   mix_dens <- dens_mat %*% t(prop)
@@ -34,40 +32,5 @@ distrib_diff_gmm <- function(x, z, prop, mu, sigma) {
     distrib_diff = distrib_diff,
     distrib_diff_vec = distrib_diff_vec,
     choice_id = choice_id
-  ))
-}
-
-#' distrib_diff_gmm_g
-#'
-#' @param x Data.
-#' @param z_g Component assignment probability vector.
-#' @param mu_g Component mean vector.
-#' @param sigma_g Component covariance matrix.
-#'
-#' @return List of
-#' * distrib_diff_g
-#' * scaled_mahalas_g
-#' * dens_g
-distrib_diff_gmm_g <- function(x, z_g, mu_g, sigma_g) {
-  var_num <- ncol(x)
-  n_g <- sum(z_g)
-
-  check_seq <- seq(0.001, 0.999, 0.001)
-  checkpoints <- stats::qbeta(check_seq, var_num / 2, (n_g - var_num - 1) / 2)
-
-  mahalas_g <- stats::mahalanobis(x, mu_g, (n_g / (n_g - 1)) * sigma_g)
-  scaled_mahalas_g <- ((n_g) / (n_g - 1)^2) * mahalas_g
-  mahala_ewcdf_g_func <- spatstat.geom::ewcdf(scaled_mahalas_g, z_g / n_g)
-
-  mahala_ewcdf_g <- mahala_ewcdf_g_func(checkpoints)
-  beta_cdf_g <- stats::pbeta(checkpoints, var_num / 2, (n_g - var_num - 1) / 2)
-  distrib_diff_g <- mean(abs(mahala_ewcdf_g - beta_cdf_g))
-
-  dens_g <- (2 * pi)^(-var_num / 2) * det(sigma_g)^(-0.5) * exp(-mahalas_g / 2)
-
-  return(list(
-    distrib_diff_g = distrib_diff_g,
-    scaled_mahalas_g = scaled_mahalas_g,
-    dens_g = dens_g
   ))
 }
