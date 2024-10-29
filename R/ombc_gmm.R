ae00ea058f8d06dc60d527bfe67e522ed8ac1e59
UltanPDoherty <ultanpdoherty@gmail.com>
2024-10-29 10:02:56 +0000

    fixed_ombc_gmm

--- R/ombc_gmm.R
+++ R/ombc_gmm.R
@@ -9,6 +9,7 @@
 #' @param gross_outs Logical vector identifying gross outliers.
 #' @param mnames Model names for mixture::gpcm.
 #' @param nmax Maximum number of iterations for mixture::gpcm.
+#' @param init_method Method used to initialise each mixture model.
 #' @param kmpp_seed Optional seed for k-means++ initialisation. Default is
 #'                  hierarchical clustering.
 #' @param print_interval How frequently the iteration count is printed.
@@ -344,4 +345,242 @@ two_set_dist <- function(x, y) {
   mean_dist <- mean(dist_mat[cbind(seq_along(lsap), lsap)])
 
   return(mean_dist)
+}
+
+# ==============================================================================
+
+#' fixed_ombc_gmm
+#'
+#' @description
+#' Iterative Detection & Identification of Outliers for a Gaussian Mixture Model
+#'
+#' @inheritParams ombc_gmm
+#' @param outlier_bool Logical vector indicating which observations are outliers
+#'                     for the fixed model.
+#'
+#' @inherit ombc_gmm return
+#'
+#' @export
+#'
+fixed_ombc_gmm <- function(
+    outlier_bool,
+    x,
+    comp_num,
+    max_out,
+    mnames = "VVV",
+    nmax = 10,
+    init_method = c("hc", "kmpp"),
+    kmpp_seed = 123,
+    print_interval = Inf) {
+  init_method <- match.arg(init_method)
+
+  expect_num <- 1
+  accept_num <- expect_num + 1
+  reject_num <- accept_num + 2
+
+  x <- as.matrix(x)
+  x0 <- x
+
+  obs_num <- nrow(x)
+
+  dist_mat0 <- as.matrix(stats::dist(x0))
+  dist_mat <- dist_mat0
+
+  track_num <- 2
+  tail_props <- expect_num / seq(obs_num, obs_num - max_out)
+
+  z <- get_init_z(
+    comp_num,
+    dist_mat = dist_mat[!outlier_bool, !outlier_bool], x = x[!outlier_bool, ],
+    init_method = init_method, kmpp_seed = kmpp_seed
+  )
+  mix <- try_mixture_gpcm(x[!outlier_bool, ], comp_num, mnames, z, nmax)
+  z <- mixture::e_step(x, mix$best_model)$z
+
+  loglike <- c()
+  removal_dens <- c()
+  distrib_diff_arr <- array(dim = c(comp_num, max_out + 1, track_num))
+  distrib_diff_mat <- matrix(nrow = max_out + 1, ncol = track_num)
+  outlier_rank_temp <- rep(0, obs_num)
+  for (i in seq_len(max_out + 1)) {
+    if (i %% print_interval == 0) cat("i = ", i, "\n")
+
+    loglike[i] <- mix$best_model$loglik
+
+    dd <- distrib_diff_gmm(
+      x,
+      z,
+      mix$best_model$model_obj[[1]]$pi_gs,
+      mix$best_model$model_obj[[1]]$mu,
+      mix$best_model$model_obj[[1]]$sigs,
+      mix$best_model$model_obj[[1]]$log_dets,
+      tail_props[i]
+    )
+
+    distrib_diff_arr[, i, ] <- dd$distrib_diff_mat
+    distrib_diff_mat[i, ] <- dd$distrib_diff_vec
+    removal_dens[i] <- dd$removal_dens
+
+    outlier_rank_temp[!outlier_rank_temp][dd$choice_id] <- i
+    x <- x[-dd$choice_id, , drop = FALSE]
+    z <- z[-dd$choice_id, , drop = FALSE]
+    dist_mat <- dist_mat[-dd$choice_id, -dd$choice_id]
+  }
+
+  outlier_rank <- double(obs_num)
+  outlier_rank <- outlier_rank_temp
+
+  outlier_num <- integer(track_num)
+
+  outlier_num[1] <- which.min(distrib_diff_mat[, 1])
+
+  reject_bool <- distrib_diff_mat[, 2] > reject_num
+  final_reject <- max(which(c(TRUE, reject_bool))) - 1
+  after_final_reject <- seq_len(max_out + 1) > final_reject
+  accept_bools <- distrib_diff_mat[, 2] < accept_num
+  outlier_num[2] <- which.max(accept_bools & after_final_reject)
+
+  outlier_num <- outlier_num - 1
+
+  outlier_bool <- matrix(nrow = obs_num, ncol = track_num)
+  mix <- list()
+  labels <- matrix(0, nrow = obs_num, ncol = track_num)
+  for (j in seq_len(track_num)) {
+    outlier_bool[, j] <- outlier_rank <= outlier_num[j] & outlier_rank != 0
+
+    z <- get_init_z(
+      comp_num,
+      dist_mat = dist_mat0[!outlier_bool[, j], !outlier_bool[, j]],
+      x = x0[!outlier_bool[, j], ],
+      init_method = init_method, kmpp_seed = kmpp_seed
+    )
+
+    mix[[j]] <- try_mixture_gpcm(
+      x0[!outlier_bool[, j], ], comp_num, mnames, z, nmax
+    )
+
+    labels[!outlier_bool[, j], j] <- mix[[j]]$map
+  }
+
+  outlier_seq <- seq(0, max_out)
+  point_size <- 1 - min(0.9, max(0, -0.1 + max_out / 250))
+
+  full <- minimum <- NULL
+  full_curve_df <- data.frame(
+    "outlier_seq" = outlier_seq,
+    "minimum" = outlier_num[1],
+    "full" = distrib_diff_mat[, 1]
+  )
+  full_curve <- full_curve_df |>
+    ggplot2::ggplot(ggplot2::aes(x = outlier_seq, y = full)) +
+    ggplot2::geom_line(
+      ggplot2::aes(colour = "full"),
+      show.legend = FALSE
+    ) +
+    ggplot2::geom_point(
+      ggplot2::aes(colour = "full"),
+      size = point_size, show.legend = FALSE
+    ) +
+    ggplot2::geom_vline(
+      ggplot2::aes(xintercept = minimum, colour = "minimum"),
+      linetype = "solid", linewidth = 0.75, show.legend = FALSE
+    ) +
+    ggplot2::scale_colour_manual(
+      values = c(full = "#000000", minimum = "#CC79A7")
+    ) +
+    ggplot2::labs(
+      title = paste0(
+        "outlierMBC (fixed model): Number of Outliers = ", outlier_num[1]
+      ),
+      x = "Outlier Number",
+      y = "Mean Absolute CDF Difference",
+      colour = ""
+    ) +
+    ggplot2::scale_x_continuous(breaks = pretty(outlier_seq))
+
+  observed <- rejection <- acceptance <- expected <- choice <- NULL
+  tail_curve_df <- data.frame(
+    "outlier_seq" = outlier_seq,
+    "observed" = distrib_diff_mat[, 2],
+    "expected" = expect_num,
+    "acceptance" = accept_num,
+    "rejection" = reject_num,
+    "choice" = outlier_num[2]
+  )
+  tail_curve <- tail_curve_df |>
+    ggplot2::ggplot(ggplot2::aes(x = outlier_seq)) +
+    ggplot2::geom_line(ggplot2::aes(y = observed, colour = "observed")) +
+    ggplot2::geom_point(
+      ggplot2::aes(y = observed, colour = "observed"),
+      size = point_size
+    ) +
+    ggplot2::geom_vline(
+      ggplot2::aes(xintercept = choice, colour = "choice"),
+      linetype = "solid", linewidth = 0.75
+    ) +
+    ggplot2::geom_hline(
+      ggplot2::aes(yintercept = rejection, colour = "rejection"),
+      linetype = "dashed", linewidth = 0.75
+    ) +
+    ggplot2::geom_hline(
+      ggplot2::aes(yintercept = acceptance, colour = "acceptance"),
+      linetype = "dashed", linewidth = 0.75
+    ) +
+    ggplot2::geom_hline(
+      ggplot2::aes(yintercept = expected, colour = "expected"),
+      linetype = "dotted", linewidth = 0.75
+    ) +
+    ggplot2::scale_colour_manual(
+      values = c(
+        observed = "#000000", expected = "#0072B2", acceptance = "#009E73",
+        choice = "#CC79A7", rejection = "#D55E00"
+      )
+    ) +
+    ggplot2::labs(
+      title = paste0(
+        "outlierMBC-tail (fixed model): Number of Outliers = ", outlier_num[2]
+      ),
+      x = "Outlier Number",
+      y = "Number of Extreme Points",
+      colour = ""
+    ) +
+    ggplot2::scale_x_continuous(breaks = pretty(outlier_seq)) +
+    ggplot2::theme(
+      legend.text = ggplot2::element_text(size = 11),
+      legend.title = ggplot2::element_text(size = 11),
+      legend.position = "bottom"
+    ) +
+    ggplot2::expand_limits(y = 0)
+
+  ombc_names <- c("full", "tail")
+  colnames(distrib_diff_mat) <- ombc_names
+  colnames(outlier_bool) <- ombc_names
+  colnames(labels) <- ombc_names
+  names(outlier_num) <- ombc_names
+  dimnames(distrib_diff_arr) <- list(
+    paste0("k", seq_len(comp_num)), NULL, ombc_names
+  )
+
+  outlier_class <- rep("normal", obs_num)
+  outlier_class[outlier_bool[, 1] & outlier_bool[, 2]] <- "out_full_&_tail"
+  outlier_class[outlier_bool[, 1] & !outlier_bool[, 2]] <- "out_full_only"
+  outlier_class[!outlier_bool[, 1] & outlier_bool[, 2]] <- "out_tail_only"
+  outlier_class <- as.factor(outlier_class)
+
+  labels <- as.data.frame(labels)
+  outlier_bool <- as.data.frame(outlier_bool)
+
+  return(list(
+    labels = labels,
+    outlier_bool = outlier_bool,
+    outlier_num = outlier_num,
+    outlier_rank = outlier_rank,
+    outlier_class = outlier_class,
+    curve_plot = full_curve,
+    tail_curve_plot = tail_curve,
+    loglike = loglike,
+    removal_dens = removal_dens,
+    distrib_diff_mat = distrib_diff_mat,
+    distrib_diff_arr = distrib_diff_arr
+  ))
 }
