523b3d50165d29d23ea774ee6325dcd06f0aeb9b
UltanPDoherty <ultanpdoherty@gmail.com>
2025-02-13 15:00:45 +0000

    Tidy and rearrange backtrack.R

--- R/backtrack.R
+++ R/backtrack.R
@@ -2,7 +2,8 @@
 #'
 #' @inheritParams ombc_gmm
 #' @param max_total_rise Value cannot exceed minimum * (1 + max_total_rise).
-#' @param max_step_rise Each step must be less than minimum * (1 + max_step_rise).
+#' @param max_step_rise
+#' Each step must be less than minimum * (1 + max_step_rise).
 #'
 #' @returns List of two lists:
 #' * minimum: $ind
@@ -46,6 +47,79 @@ backtrack <- function(x, max_total_rise = 0.1, max_step_rise = 0.05) {
   return(list("minimum" = minimum, "backtrack" = backtrack))
 }
 
+#' Plot the outlier number selection curve for the backtrack method.
+#'
+#' @inheritParams plot_full_curve
+#' @inheritParams backtrack
+#'
+#' @returns A gg object.
+#' @export
+plot_backtrack_curve <- function(
+    ombc_out, max_total_rise = 0.1, max_step_rise = 0.05) {
+  gross_num <- sum(ombc_out$gross_outs)
+  max_out <- max(ombc_out$outlier_rank) - 1
+  outlier_num <- ombc_out$outlier_num
+  distrib_diff_mat <- ombc_out$distrib_diff_mat
+
+  backtrack_num <- backtrack(
+    ombc_out$distrib_diff_mat[, "full"], max_total_rise, max_step_rise
+  )$backtrack$ind + gross_num - 1
+
+  outlier_seq <- seq(gross_num, max_out)
+  point_size <- 1 - min(0.9, max(0, -0.1 + max_out / 250))
+
+  backtrack <- minimum <- choice <- NULL
+  backtrack_curve_df <- data.frame(
+    "outlier_seq" = outlier_seq,
+    "minimum" = as.integer(outlier_num["full"]),
+    "choice" = as.integer(backtrack_num),
+    "backtrack" = distrib_diff_mat[, "full"] / min(distrib_diff_mat[, "full"])
+  )
+  backtrack_curve <- backtrack_curve_df |>
+    ggplot2::ggplot(ggplot2::aes(x = outlier_seq, y = backtrack)) +
+    ggplot2::geom_line(
+      ggplot2::aes(colour = "backtrack"),
+      show.legend = FALSE
+    ) +
+    ggplot2::geom_point(
+      ggplot2::aes(colour = "backtrack"),
+      size = point_size, show.legend = FALSE
+    ) +
+    ggplot2::geom_vline(
+      ggplot2::aes(xintercept = choice, colour = "choice"),
+      linetype = "solid", linewidth = 0.75, show.legend = FALSE
+    ) +
+    ggplot2::geom_vline(
+      ggplot2::aes(xintercept = minimum, colour = "minimum"),
+      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
+    ) +
+    ggplot2::geom_hline(
+      ggplot2::aes(
+        yintercept = min(backtrack) * (1 + max_total_rise), colour = "minimum"
+      ),
+      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
+    ) +
+    ggplot2::scale_colour_manual(
+      values = c(backtrack = "#000000", choice = "#CC79A7", minimum = "#009E73")
+    ) +
+    ggplot2::labs(
+      title =
+        paste0("outlierMBC: Number of Outliers = ", backtrack_num),
+      subtitle = paste0(
+        "max_total_rise = ", max_total_rise, ",
+        max_step_rise = ", max_step_rise
+      ),
+      x = "Outlier Number",
+      y = "Rescaled Mean Absolute CDF Difference",
+      colour = ""
+    ) +
+    ggplot2::scale_x_continuous(breaks = pretty(outlier_seq)) +
+    ggplot2::expand_limits(y = 0)
+
+  return(backtrack_curve)
+}
+
+# ------------------------------------------------------------------------------
 
 #' Fit a GMM to the backtrack solution.
 #'
@@ -71,9 +145,11 @@ backtrack <- function(x, max_total_rise = 0.1, max_step_rise = 0.05) {
 #' backtrack_gmm(gmm_k3n1000o10[, 1:2], ombc_gmm_k3n1000o10, 0.1, 0.01)
 backtrack_gmm <- function(
     x, ombc_out,
-    max_total_rise = 0.1, max_step_rise = 0.05, init_model = NULL, init_z = NULL) {
-  backtrack_out <-
-    backtrack(ombc_out$distrib_diff_mat[, "full"], max_total_rise, max_step_rise)
+    max_total_rise = 0.1, max_step_rise = 0.05,
+    init_model = NULL, init_z = NULL) {
+  backtrack_out <- backtrack(
+    ombc_out$distrib_diff_mat[, "full"], max_total_rise, max_step_rise
+  )
 
   x0 <- as.matrix(x)
 
@@ -136,72 +212,4 @@ backtrack_gmm <- function(
     "outlier_num" = outlier_num,
     "mix" = mix
   ))
-}
-
-#' Plot the outlier number selection curve for the backtrack method.
-#'
-#' @inheritParams plot_full_curve
-#' @inheritParams backtrack
-#'
-#' @returns A gg object.
-#' @export
-plot_backtrack_curve <- function(ombc_out, max_total_rise = 0.1, max_step_rise = 0.05) {
-  gross_num <- sum(ombc_out$gross_outs)
-  max_out <- max(ombc_out$outlier_rank) - 1
-  outlier_num <- ombc_out$outlier_num
-  distrib_diff_mat <- ombc_out$distrib_diff_mat
-
-  backtrack_num <- backtrack(
-    ombc_out$distrib_diff_mat[, "full"], max_total_rise, max_step_rise
-  )$backtrack$ind + gross_num - 1
-
-  outlier_seq <- seq(gross_num, max_out)
-  point_size <- 1 - min(0.9, max(0, -0.1 + max_out / 250))
-
-  backtrack <- minimum <- choice <- NULL
-  backtrack_curve_df <- data.frame(
-    "outlier_seq" = outlier_seq,
-    "minimum" = as.integer(outlier_num["full"]),
-    "choice" = as.integer(backtrack_num),
-    "backtrack" = distrib_diff_mat[, "full"] / min(distrib_diff_mat[, "full"])
-  )
-  backtrack_curve <- backtrack_curve_df |>
-    ggplot2::ggplot(ggplot2::aes(x = outlier_seq, y = backtrack)) +
-    ggplot2::geom_line(
-      ggplot2::aes(colour = "backtrack"),
-      show.legend = FALSE
-    ) +
-    ggplot2::geom_point(
-      ggplot2::aes(colour = "backtrack"),
-      size = point_size, show.legend = FALSE
-    ) +
-    ggplot2::geom_vline(
-      ggplot2::aes(xintercept = choice, colour = "choice"),
-      linetype = "solid", linewidth = 0.75, show.legend = FALSE
-    ) +
-    ggplot2::geom_vline(
-      ggplot2::aes(xintercept = minimum, colour = "minimum"),
-      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
-    ) +
-    ggplot2::geom_hline(
-      ggplot2::aes(
-        yintercept = min(backtrack) * (1 + max_total_rise), colour = "minimum"
-      ),
-      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
-    ) +
-    ggplot2::scale_colour_manual(
-      values = c(backtrack = "#000000", choice = "#CC79A7", minimum = "#009E73")
-    ) +
-    ggplot2::labs(
-      title =
-        paste0("outlierMBC: Number of Outliers = ", backtrack_num),
-      subtitle = paste0("max_total_rise = ", max_total_rise, ", max_step_rise = ", max_step_rise),
-      x = "Outlier Number",
-      y = "Rescaled Mean Absolute CDF Difference",
-      colour = ""
-    ) +
-    ggplot2::scale_x_continuous(breaks = pretty(outlier_seq)) +
-    ggplot2::expand_limits(y = 0)
-
-  return(backtrack_curve)
 }
