d9264215af79fb8fe7ad57f645e4aa0852cdf2d4
UltanPDoherty <ultanpdoherty@gmail.com>
2025-02-28 10:11:14 +0000

    plot_backtrack: add legend

--- R/backtrack.R
+++ R/backtrack.R
@@ -58,7 +58,7 @@ plot_backtrack <- function(
     ombc_out, max_total_rise = 0.1, max_step_rise = 0.05) {
   gross_num <- sum(ombc_out$gross_outs)
   max_out <- max(ombc_out$outlier_rank) - 1
-  outlier_num <- ombc_out$outlier_num
+  minimum_num <- ombc_out$outlier_num
   distrib_diff_vec <- ombc_out$distrib_diff_vec
 
   backtrack_num <- backtrack(
@@ -68,53 +68,52 @@ plot_backtrack <- function(
   outlier_seq <- seq(gross_num, max_out)
   point_size <- 1 - min(0.9, max(0, -0.1 + max_out / 250))
 
-  backtrack <- minimum <- choice <- NULL
+  backtrack <- value <- label <- NULL
   backtrack_curve_df <- data.frame(
     "outlier_seq" = outlier_seq,
-    "minimum" = as.integer(outlier_num),
-    "choice" = as.integer(backtrack_num),
     "backtrack" = distrib_diff_vec / min(distrib_diff_vec)
+  )
+  vlines_df <- data.frame(
+    "label" = c("backtrack solution", "minimum solution"),
+    "value" = c(as.integer(backtrack_num), as.integer(minimum_num))
+  )
+  hlines_df <- data.frame(
+    "label" = "1 + max_total_rise",
+    "value" = 1 + max_total_rise
+  )
+  colour_scheme <- c(
+    "backtrack" = "#000000", "minimum solution" = "#CC79A7",
+    "backtrack solution" = "#009E73", "1 + max_total_rise" = "#0072B2"
+  )
+  linetype_scheme <- c(
+    "minimum solution" = "dashed", "backtrack solution" = "solid",
+    "1 + max_total_rise" = "dotted"
   )
   backtrack_curve <- backtrack_curve_df |>
     ggplot2::ggplot(ggplot2::aes(x = outlier_seq, y = backtrack)) +
-    ggplot2::geom_line(
-      ggplot2::aes(colour = "backtrack"),
-      show.legend = FALSE
-    ) +
-    ggplot2::geom_point(
-      ggplot2::aes(colour = "backtrack"),
-      size = point_size, show.legend = FALSE
-    ) +
+    ggplot2::geom_line(show.legend = FALSE) +
+    ggplot2::geom_point(size = point_size, show.legend = FALSE) +
     ggplot2::geom_vline(
-      ggplot2::aes(xintercept = choice, colour = "choice"),
-      linetype = "solid", linewidth = 0.75, show.legend = FALSE
-    ) +
-    ggplot2::geom_vline(
-      ggplot2::aes(xintercept = minimum, colour = "minimum"),
-      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
+      data = vlines_df, linewidth = 0.75,
+      ggplot2::aes(xintercept = value, colour = label, linetype = label)
     ) +
     ggplot2::geom_hline(
-      ggplot2::aes(
-        yintercept = min(backtrack) * (1 + max_total_rise), colour = "minimum"
-      ),
-      linetype = "dashed", linewidth = 0.75, show.legend = FALSE
+      data = hlines_df, linewidth = 0.75,
+      ggplot2::aes(yintercept = value, colour = label, linetype = label)
     ) +
-    ggplot2::scale_colour_manual(
-      values = c(backtrack = "#000000", choice = "#CC79A7", minimum = "#009E73")
-    ) +
+    ggplot2::scale_colour_manual(values = colour_scheme) +
+    ggplot2::scale_linetype_manual(values = linetype_scheme) +
     ggplot2::labs(
-      title =
-        paste0("outlierMBC: Number of Outliers = ", backtrack_num),
+      title = paste0("outlierMBC: Number of Outliers = ", backtrack_num),
       subtitle = paste0(
-        "max_total_rise = ", max_total_rise,
-        ", max_step_rise = ", max_step_rise
+        "max_total_rise = ", max_total_rise, ", max_step_rise = ", max_step_rise
       ),
-      x = "Outlier Number",
-      y = "Rescaled Dissimilarity",
-      colour = ""
+      x = "Outlier Number", y = "Rescaled Dissimilarity",
+      colour = "", linetype = ""
     ) +
     ggplot2::scale_x_continuous(breaks = pretty(outlier_seq)) +
-    ggplot2::expand_limits(y = 0)
+    ggplot2::expand_limits(y = 0) +
+    ggplot2::theme(legend.position = "bottom")
 
   return(backtrack_curve)
 }
