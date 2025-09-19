e20d7569e84a901cf4d464eb89c70521639bfaf4
UltanPDoherty <ultanpdoherty@gmail.com>
2025-09-19 17:10:25 +0100

    distrib_diff_residual: undo std. dev. change

--- R/distrib_diff.R
+++ R/distrib_diff.R
@@ -288,7 +288,8 @@ distrib_diff_residual <- function(
   hat_g <- stats::hatvalues(mod_g)
 
   sqrt_weighted_rss_g <- sqrt(n_g) * weighted_rmse_g
-  sigma_hat_g <- sqrt_weighted_rss_g / sqrt(df_g)
+  sigma_hat_g <- sqrt_weighted_rss_g / sqrt(n_g)
+
   student_resids_g <- mod_g$residuals / (sigma_hat_g * sqrt(1 - hat_g))
   scsqst_res_g <- student_resids_g^2 / df_g
   scsqst_res_ewcdf_g_func <- spatstat.univar::ewcdf(scsqst_res_g, z_g / n_g)
